f3ef32d78d9b24998dfbe687461ff87ae9fc5909
孙一 <sunyiex@163.com>
2024-08-13 17:48:16 +0800

    Update config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     COOKIE: process.env.COOKIE || '',
+    CHECK_PARAM: process.env.CHECK_PARAM || '?aid=2608&uuid=6953442233299682829&spider=0&msToken=kXzCpKz3CUD2tI9Dm9oEGGxbsmQZ6oHUPIv_FdkgU0bgUDx3cU3hvhaJbXychFN-UAlKtb12N4yE_OMwcY2TQqFjR0dAfqKvB4juIr2g93w5FHuqWp3Le6U0BoffDqM%3D&a_bogus=DfsOXOZVMsm1SwVdzwkz9jDEJhD0YW5%2FgZENEmthStLD',
     TOKEN: process.env.TOKEN || '',
     DINGTALK_WEBHOOK: process.env.DINGTALK_WEBHOOK || '',
     DINGTALK_SECRET: process.env.DINGTALK_SECRET || '',
